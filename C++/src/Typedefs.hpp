a0535474b076efbfe7a40ac0321b2cab4dd2325c
CatShot112 <12834417+CatShot112@users.noreply.github.com>
2023-01-12 23:26:05 +0100

    Add more stuff to Typedefs

--- C++/src/Typedefs.hpp
+++ C++/src/Typedefs.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 #include <cstdint>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+using std::unordered_map;
+using std::vector;
+using std::string;
 
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t u8;
+
+typedef float f32;
+typedef double f64;
